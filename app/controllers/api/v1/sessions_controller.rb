5d0cd4877f5f565b150b7448def67209573e1cea
Michal Szmyd <michal.szmyd@icloud.com>
2019-03-16 12:03:06 +0100

    Skip csrf token for sessions create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class SessionsController < Devise::SessionsController
+      skip_before_action :verify_authenticity_token
+      respond_to :json
+
       def create
         user = User.find_by(email: session_params[:email])
 
